885ebc4d92068bfd71eeb720cda9896228a01f74
ongjoshua <joshuaspencer.ong.iics@ust.edu.ph>
2021-02-18 14:46:01 +0800

    added form response to add admin

--- src/app/admin/accounts-new-admin/accounts-new-admin.component.ts
+++ src/app/admin/accounts-new-admin/accounts-new-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-accounts-new-admin',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsNewAdminComponent implements OnInit {
 
+  addAccountForm: FormGroup;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit()
+  {
+    this.addAccountForm = new FormGroup(
+      {
+        "name": new FormControl(null, [Validators.required]),
+        "email": new FormControl(null, [Validators.email, Validators.required]),
+        "contactNumber": new FormControl(null, [Validators.email]),
+        "address": new FormControl(null, [Validators.required]),
+        "password": new FormControl(null, [Validators.required, Validators.minLength(6)])
+      }
+    );
   }
 
+  onSubmit()
+  {
+
+  }
 }
